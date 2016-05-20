3da7c841f9382d0c62242a1b718511acec97e9f7
Profpatsch <mail@profpatsch.de>
2016-05-20 05:57:51 +0200

    release.nix: manual elf patching magic for debian

--- release.nix
+++ release.nix
@@ -2,44 +2,57 @@ let
   np = import <nixpkgs> {};
 
   hp = np.haskellPackages.override {
-    overrides = (self: super: {
-      openlabGitit = np.haskell.lib.overrideCabal super.gitit (drv: {
+    overrides = with np.haskell.lib; (self: super: {
+      openlabGitit = disableSharedExecutables (overrideCabal super.gitit (drv: {
         src = np.fetchFromGitHub {
           owner = "openlab-aux";
           repo = "gitit";
-          rev = "e6e69d5c1a16e196416ce096ce3e006dabd6647b";
-          sha256 = "0jgkw4i21fgq2fh54y3lv0kx0q7cw6rvbwvyzapngv2brf5s45k8";
+          rev = "f787065a5462f1f3f9e906f2665dec8c3da56468";
+          sha256 = "1fd04iq24rzhpjmyfgr9xygwp75id329pscgv30zxvqkyw0j2r9y";
         };
-      });
+      }));
     });
   };
-in
-{
+
   openlabGitit = hp.openlabGitit;
 
-  deb = with np;
-    releaseTools.debBuild (hp.openlabGitit.drvAttrs // {
-      diskImage = vmTools.diskImageFuns.debian7x86_64 {
-        packagesList = fetchurl {
-          url = http://ftp.de.debian.org/debian/dists/wheezy/main/binary-amd64/Packages.bz2;
-          sha256 = "1kir3j6y81s914njvs0sbwywq7qv28f8s615r9agg9s0h5g760fw";
-        };
-      };
-      memSize = 16384;
-      QEMU_OPTS = "-smp 12";
-      configureFlags = [ "--prefix=/usr" ];
-      installCommand = "./Setup install";
-    });
+  # deb = with np;
+  #   releaseTools.debBuild (hp.openlabGitit.drvAttrs // {
+  #     diskImage = vmTools.diskImageFuns.debian7x86_64 {
+  #       packagesList = fetchurl {
+  #         url = http://ftp.de.debian.org/debian/dists/wheezy/main/binary-amd64/Packages.bz2;
+  #         sha256 = "1kir3j6y81s914njvs0sbwywq7qv28f8s615r9agg9s0h5g760fw";
+  #       };
+  #     };
+  #     memSize = 16384;
+  #     QEMU_OPTS = "-smp 12";
+  #     configureFlags = [ "--prefix=/usr" ];
+  #     installCommand = "./Setup install";
+  #   });
 
-  test = with np;
-    releaseTools.debBuild (hello.drvAttrs // {
-      diskImage = vmTools.diskImageFuns.debian7x86_64 {
-        packagesList = fetchurl {
-          url = http://ftp.de.debian.org/debian/dists/wheezy/main/binary-amd64/Packages.bz2;
-          sha256 = "1kir3j6y81s914njvs0sbwywq7qv28f8s615r9agg9s0h5g760fw";
-        };
-      };
-      memSize = 16384;
-      QEMU_OPTS = "-smp 12";
-    });
-}
+  deb =
+    let debian_sonames = [
+      { from = "liblua.so.5.1";
+        to   = "liblua5.1.so.0.0.0"; }
+      { from = "libpcre.so.1";
+        to   = "libpcre.so.3"; }
+    ];
+        replace = e: ''--replace-needed "${e.from}" "${e.to}"'';
+        patchelf = file: arg: ''patchelf ${arg} ${file}'';
+    in np.runCommand "gitit-debian" { src = hp.openlabGitit; } ''
+      mkdir $out
+      cp -r "$src"/* "$out"
+      chmod -R +w "$out/bin"
+      ${np.lib.concatMapStringsSep "\n" (patchelf "$out/bin/gitit") ([
+        ''--set-interpreter "/lib64/ld-linux-x86-64.so.2"''
+        ''--set-rpath ""''
+      ] ++ map replace debian_sonames)}
+  '';
+
+  debTar = np.runCommand "gitit-debian-tar" { src = deb; } ''
+    mkdir $out && cd $src
+    tar czf $out/gitit.tar.gz *
+  '';
+
+in
+  { inherit openlabGitit deb debTar; }
