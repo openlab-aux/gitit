73efc6592cdf458d255106ff7806342062644c49
sternenseemann <git@lukasepple.de>
2016-08-29 16:36:46 +0200

    Use more pragmatic default.nix for now

--- default.nix
+++ default.nix
@@ -1,27 +1,2 @@
-with import <nixpkgs> {};
-# { haskell, haskellPackages, fetchFromGitHub, makeWrapper }:
-
-let
-  mde = fetchFromGitHub {
-    owner = "NextStepWebs";
-    repo = "simplemde-markdown-editor";
-    rev = "1.11.2";
-    sha256 = "0pzbyg7vl7bk7wlwgcmch9zxw04f4hakysza9xq0iyrnal22ra9i";
-  };
-
-  deps = "${mde}/dist;
-
-  hp = haskellPackages.override {
-    overrides = with haskell.lib; (self: super: {
-      openlabGitit = overrideCabal super.gitit (drv: {
-        src = ./.;
-        buildDepends = drv.buildDepends or [] ++ [ makeWrapper ];
-        postInstall = drv.postInstall or "" + ''
-          wrapProgram $out/bin/gitit --set gitit_deps ${deps}
-        '';
-      });
-    });
-  };
-
-in
-  hp.openlabGitit
+let nixpkgs =  import <nixpkgs> {};
+in nixpkgs.haskellPackages.callPackage ./gitit.nix {}
