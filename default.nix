b32cfa4a25f8cde31ca75f3bb3501c8647f06a54
sternenseemann <git@lukasepple.de>
2020-06-12 19:44:48 +0200

    Fix nix expressions for gitit 0.13.0.0

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 {
   nixpkgsSrc ? fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/1af8f3a980bb8ac92f5c09ac23cca4781571bcd1.tar.gz";
-    sha256 = "1ak1kanmq1qw0r6bbk9fpz30p8dlifcc05mcyri6il7fj0cbplp1";
+    url = "https://github.com/NixOS/nixpkgs/archive/91340aeea87aa25f8265b0a69af88d7d36fcf04f.tar.gz";
+    sha256 = "0npma33j6a7cx6gmi4x8acxlffp482dfv2l4acrmpadv2v7nayzi";
   }
 }:
 
@@ -11,20 +11,21 @@ let
   hp = nixpkgs.haskellPackages.override {
     overrides = self: super: with nixpkgs.haskell.lib; {
 
-      hoauth2 = doJailbreak super.hoauth2;
-      skylighting-core = overrideCabal super.skylighting-core (_: {
-        version = "0.7.4";
-        sha256 = "1awddq9cn5gyafz97ir21rncq97k2gzfxijn2xmxw35qhr2kbfl0";
-      });
-      skylighting = overrideCabal super.skylighting (_: {
-        version = "0.7.4";
-        sha256 = "0w1cv21rm4ssmr2zbn7bamlfc2pkswxg5plvqzrf7rs4h1y43672";
-      });
 
-      gitit = overrideCabal (self.callPackage ./gitit.nix {}) (old: {
-        buildTools = with self; [ cabal-install ghcid ];
+      gitit = super.gitit.overrideAttrs (old: {
+        src = nixpkgs.nix-gitignore.gitignoreSource [ ".git/" ] ./.;
       });
     };
   };
+
+#      hoauth2 = doJailbreak super.hoauth2;
+#      skylighting-core = overrideCabal super.skylighting-core (_: {
+#        version = "0.7.4";
+#        sha256 = "1awddq9cn5gyafz97ir21rncq97k2gzfxijn2xmxw35qhr2kbfl0";
+#      });
+#      skylighting = overrideCabal super.skylighting (_: {
+#        version = "0.7.4";
+#        sha256 = "0w1cv21rm4ssmr2zbn7bamlfc2pkswxg5plvqzrf7rs4h1y43672";
+#      });
 
 in hp.gitit
