9afb577cc575fea49f39e4513051adc5b8eacb0e
Profpatsch <mail@profpatsch.de>
2024-10-11 16:52:23 +0200

    chore(EasyMDE): format file

--- data/static/js/markdown-editor.js
+++ data/static/js/markdown-editor.js
@@ -1,34 +1,23 @@
 // include the EasyMDE CSS dynamically (since it doesn't make sense without JS)
-//
-//var cssId = 'EasyMDECSS'
-//if (!document.getElementById(cssId))
-//{
-//    var head  = document.getElementsByTagName('head')[0];
-//    var link  = document.createElement('link');
-//    link.id   = cssId;
-//    link.rel  = 'stylesheet';
-//    link.type = 'text/css';
-//    link.href = '/static/easymde.min.js';
-//    link.media = 'all';
-//    head.appendChild(link);
-//}
 
-var easymde = new EasyMDE({ element: document.getElementById("editedText"), forceSync: true, autoDownloadFontAwesome : false, spellChecker : false });
+var easymde = new EasyMDE({
+  element: document.getElementById("editedText"),
+  forceSync: true,
+  autoDownloadFontAwesome: false,
+  spellChecker: false,
+});
 
 // update the preview on every change, after `changeTimeout` ms.
-(function (){
-    var changeTimeout = 1 *1000;
-    var noTimeout = true;
-    easymde.codemirror.on("change", function(chgs){
-        if (noTimeout === true) {
-            noTimeout = false;
-            window.setTimeout(
-                function(){
-                    noTimeout = true;
-                    updatePreviewPane();
-                },
-                changeTimeout
-            );
-        };
-    });
+(function () {
+  var changeTimeout = 1 * 1000;
+  var noTimeout = true;
+  easymde.codemirror.on("change", function (chgs) {
+    if (noTimeout === true) {
+      noTimeout = false;
+      window.setTimeout(function () {
+        noTimeout = true;
+        updatePreviewPane();
+      }, changeTimeout);
+    }
+  });
 })();
